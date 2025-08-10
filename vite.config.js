b651a8196626637f38312e6b1a7dcd8b2e64dfad
raihan <raihanamin2212@gmail.com>
2025-08-10 20:21:13 +0700

    [RHN-01] - init project

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  base: '/resume/',
+  plugins: [vue()],
+  resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  }
+})
